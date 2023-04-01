795c922e5d708eb2df58f12b9bf9c5f9b08833e8
shadiashafi <shadia.shafi@gmail.com>
2023-04-01 11:18:23 +0530

    modification

---
+++ css/javascript/intro.js
@@ -0,0 +1,8 @@
+document.write('axsldsrj <br/>');
+a=10;
+b="10";
+document.write(typeof(a));
+document.write(typeof(b));
+document.write("<br> <br> " + a + b + "<br> <br>");
+console.log("asfetete");
+alert("ddfg");
